125dfe1160cab4d230231c21247bdb724b0f8e87
LeiWang1999 <leiwang1999@outlook.com>
2023-05-08 07:30:55 -0800

    re-construct for enabled quantization.

--- end2end/text-generation.py
+++ end2end/text-generation.py
@@ -8,6 +8,7 @@ from auto_gptq import AutoGPTQForCausalLM, BaseQuantizeConfig
 pretrained_model_dir = "facebook/opt-66b"
 quantized_model_dir = "quantization/models/opt-66b-3bit"
 
+enable_quantize = False
 
 tokenizer = AutoTokenizer.from_pretrained(pretrained_model_dir, use_fast=True)
 examples = [
@@ -16,22 +17,23 @@ examples = [
     )
 ]
 
-quantize_config = BaseQuantizeConfig(
-    bits=3,  # quantize model to 3-bit
-    # desc_act=False,  # disable activation description
-    # group_size=128,  # disable group quantization
-    desc_act=True
-)
 
-# load un-quantized model, the model will always be force loaded into cpu
-model = AutoGPTQForCausalLM.from_pretrained(pretrained_model_dir, quantize_config)
+if enable_quantize:
+    quantize_config = BaseQuantizeConfig(
+        bits=3,  # quantize model to 3-bit
+        # desc_act=False,  # disable activation description
+        # group_size=128,  # disable group quantization
+        desc_act=True
+    )
+    # load un-quantized model, the model will always be force loaded into cpu
+    model = AutoGPTQForCausalLM.from_pretrained(pretrained_model_dir, quantize_config)
 
-# quantize model, the examples should be list of dict whose keys can only be "input_ids" and "attention_mask" 
-# with value under torch.LongTensor type.
-model.quantize(examples, use_tvm=True)
+    # quantize model, the examples should be list of dict whose keys can only be "input_ids" and "attention_mask" 
+    # with value under torch.LongTensor type.
+    model.quantize(examples, use_tvm=True)
 
-# save quantized model
-model.save_quantized(quantized_model_dir)
+    # save quantized model
+    model.save_quantized(quantized_model_dir)
 
 # load quantized model to the first GPU
 model = AutoGPTQForCausalLM.from_quantized(quantized_model_dir, device="cuda:0", use_tvm=True)
